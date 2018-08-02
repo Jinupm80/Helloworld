e60264db2d7435ac386d71efc561bc33940d3eed
Mathew, Jinu <jinu.p.m80@gmail.com>
2018-08-03 01:03:53 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    /*comments
+    *?
+     */
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello world");
+    }
+}
